95bdf2116fef0d3a2bf420097a59a1707070b877
naustinapril <nageshperuri@outlook.com>
2025-04-25 01:19:36 +0530

    Day2

---
+++ Day2.py
@@ -0,0 +1,24 @@
+#Agenda for today topics
+#string  data type
+name='nag'
+print(type(name))
+x="naa"
+print(type(x))
+name=" 'nagesh'\t reddy"
+print(name)
+print(type(name))
+name = 'subash " chandra" bose'
+print(name)
+
+#multiline string literals
+a= '''sri basararama college
+                          tirupai'''
+print(a)
+#using index in string
+a='ramesh anna'
+print(a[0])
+print(a[-2])
+print(a[4])
+#slice operators
+#len function
+#positive indexing and negative indexing
--- day1.py
+++ day1.py
@@ -1,1 +1,13 @@
+#identifiers
 a=20;b=30 ;print(a+b)
+#how to find the data type
+a="30"
+print(type(a))
+a=True
+print(type(a))
+#how to find the address of the variable
+a=40
+print(id(a))
+#functions are print,id,type
+a=23.09
+print(type(a))
